ebbaec4151ffc7dd51006d8ea3633a72be2d993a
Joe Grandja <jgrandja@vmware.com>
2023-10-01 21:30:51 +0200

    Introspect authorities for opaque access tokens

--- messages-resource/src/main/java/sample/config/ResourceServerConfig.java
+++ messages-resource/src/main/java/sample/config/ResourceServerConfig.java
@@ -1,5 +1,6 @@
 package sample.config;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -7,14 +8,20 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.core.OAuth2TokenIntrospectionClaimNames;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.CollectionUtils;
 
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
@@ -31,7 +38,8 @@ public class ResourceServerConfig {
 			)
 			.oauth2ResourceServer(resourceServer ->
 				resourceServer
-					.opaqueToken(Customizer.withDefaults())
+					.opaqueToken(opaqueToken ->
+						opaqueToken.authenticationConverter(opaqueTokenAuthenticationConverter()))
 			);
 		return http.build();
 	}
@@ -58,6 +66,35 @@ public class ResourceServerConfig {
 			authorities.addAll(customAuthoritiesConverter.convert(jwt));
 			return authorities;
 		};
+	}
+
+	private OpaqueTokenAuthenticationConverter opaqueTokenAuthenticationConverter() {
+		return (introspectedToken, authenticatedPrincipal) -> {
+			Instant iat = authenticatedPrincipal.getAttribute(OAuth2TokenIntrospectionClaimNames.IAT);
+			Instant exp = authenticatedPrincipal.getAttribute(OAuth2TokenIntrospectionClaimNames.EXP);
+			OAuth2AccessToken accessToken = new OAuth2AccessToken(
+					OAuth2AccessToken.TokenType.BEARER, introspectedToken, iat, exp);
+
+			List<GrantedAuthority> authorities = extractAuthoritiesFromClaims(authenticatedPrincipal);
+
+			return new BearerTokenAuthentication(authenticatedPrincipal, accessToken, authorities);
+		};
+	}
+
+	private List<GrantedAuthority> extractAuthoritiesFromClaims(OAuth2AuthenticatedPrincipal authenticatedPrincipal) {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+
+		List<String> scopeClaim = authenticatedPrincipal.getAttribute(OAuth2TokenIntrospectionClaimNames.SCOPE);
+		if (!CollectionUtils.isEmpty(scopeClaim)) {
+			scopeClaim.forEach(scope -> authorities.add(new SimpleGrantedAuthority("SCOPE_" + scope)));
+		}
+
+		List<String> authoritiesClaim = authenticatedPrincipal.getAttribute("authorities");
+		if (!CollectionUtils.isEmpty(authoritiesClaim)) {
+			authoritiesClaim.forEach(authority -> authorities.add(new SimpleGrantedAuthority(authority)));
+		}
+
+		return authorities;
 	}
 
 }
