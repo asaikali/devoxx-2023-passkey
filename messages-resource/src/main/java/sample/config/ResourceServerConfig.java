a1aa673c967d781aafa6f5946d595309da09bfb9
Joe Grandja <jgrandja@vmware.com>
2023-10-01 21:12:53 +0200

    Add role-based authorization to resource server

--- messages-resource/src/main/java/sample/config/ResourceServerConfig.java
+++ messages-resource/src/main/java/sample/config/ResourceServerConfig.java
@@ -1,10 +1,18 @@
 package sample.config;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -18,13 +26,38 @@ public class ResourceServerConfig {
 			.securityMatcher("/messages/**")
 				.authorizeHttpRequests(authorize ->
 					authorize
-						.requestMatchers("/messages/**").hasAuthority("SCOPE_message.read")
+						.requestMatchers("/messages/**").hasAnyAuthority("SCOPE_message.read", "ROLE_user")
 			)
 			.oauth2ResourceServer(resourceServer ->
-					resourceServer.jwt(Customizer.withDefaults())
+				resourceServer
+					.jwt(jwt ->
+						jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
 			);
 		return http.build();
 	}
 	// @formatter:on
+
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter());
+		return jwtAuthenticationConverter;
+	}
+
+	private Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter() {
+		// Use 'scope' or 'scp' claim (the default) to extract authorities
+		JwtGrantedAuthoritiesConverter defaultAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+		// Use 'authorities' claim to extract authorities
+		JwtGrantedAuthoritiesConverter customAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		customAuthoritiesConverter.setAuthorityPrefix("");
+		customAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+
+		return (jwt) -> {
+			List<GrantedAuthority> authorities = new ArrayList<>();
+			authorities.addAll(defaultAuthoritiesConverter.convert(jwt));
+			authorities.addAll(customAuthoritiesConverter.convert(jwt));
+			return authorities;
+		};
+	}
 
 }
