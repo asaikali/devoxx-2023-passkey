8b423eb0af888921c51f7120b2082669086ee438
Joe Grandja <jgrandja@vmware.com>
2023-10-01 21:26:38 +0200

    Add user authorities as custom claim in opaque access tokens

--- default-authorizationserver/src/main/java/sample/config/AuthorizationServerConfig.java
+++ default-authorizationserver/src/main/java/sample/config/AuthorizationServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenClaimsContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 @Configuration(proxyBeanMethods = false)
@@ -27,6 +28,18 @@ public class AuthorizationServerConfig {
 
 	@Bean
 	public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
+		return (context) -> {
+			if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+				context.getClaims().claims((claims) -> {
+					Set<String> authorities = AuthorityUtils.authorityListToSet(context.getPrincipal().getAuthorities());
+					claims.put("authorities", authorities);
+				});
+			}
+		};
+	}
+
+	@Bean
+	public OAuth2TokenCustomizer<OAuth2TokenClaimsContext> opaqueTokenCustomizer() {
 		return (context) -> {
 			if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
 				context.getClaims().claims((claims) -> {
