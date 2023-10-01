f941d7b325217b84cc3c12d7775bc995a438079d
Joe Grandja <jgrandja@vmware.com>
2023-10-01 20:35:19 +0200

    Disable spring-boot-starter-oauth2-authorization-server

--- default-authorizationserver/src/main/java/sample/DefaultAuthorizationServerApplication.java
+++ default-authorizationserver/src/main/java/sample/DefaultAuthorizationServerApplication.java
@@ -2,8 +2,9 @@ package sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.server.servlet.OAuth2AuthorizationServerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = OAuth2AuthorizationServerAutoConfiguration.class )
 public class DefaultAuthorizationServerApplication {
 
 	public static void main(String[] args) {
